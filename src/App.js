a260cac2afb9d8985d789a4260882d9110e523bf
Simoncods <admin@uncensoredwears.com>
2020-06-15 13:45:52 +0100

    commit on 'fintech'

--- src/App.js
+++ src/App.js
@@ -18,11 +18,11 @@ import Link from '@material-ui/core/Link';
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {'Copyright © 2020 '}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+      AccessTech Bills
       </Link>{' '}
-      {new Date().getFullYear()}
+      
       {'.'}
     </Typography>
   );
@@ -192,10 +192,10 @@ export default function Album() {
       {/* Footer */}
       <footer className={classes.footer}>
         <Typography variant="h6" align="center" gutterBottom>
-          Footer
+         
         </Typography>
         <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-          Something here to give the footer a purpose!
+         
         </Typography>
         <Copyright />
       </footer>
