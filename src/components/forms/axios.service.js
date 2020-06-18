ea12e7cf6b13018d6e7bb739f60fdc608f0a443d
Aj <jayanwana@gmail.com>
2020-06-18 18:42:41 +0100

    use proxy server

--- src/components/forms/axios.service.js
+++ src/components/forms/axios.service.js
@@ -10,13 +10,22 @@ class AccessTechApi {
     this.password = password
     this.source = axios.CancelToken.source
     this.api = axios.create({
-      baseURL: "http://104.200.21.110:8181/api",
+      baseURL: "http://accessbills-proxy.herokuapp.com",
+      // baseURL: "http://localhost:3001",
       headers: {
+        "Access-Control-Allow-Origin": "*",
         "Content-Type": "application/json"
       }
     });
-    this.token = this.authenticate()
-    this.headers = {
+    this.authenticate().then(response => {
+     // console.log(response.data);
+     this.token = response.data.token
+   }).catch(error => console.log(error))
+    // console.log(this.token);
+  }
+
+  headers() {
+    return {
       headers: {
         "Content-type": "application/json",
         "Authorization": `Bearer ${this.token}`
@@ -24,16 +33,13 @@ class AccessTechApi {
     }
   }
 
-  authenticate() {
+  async authenticate() {
     const data = {username: this.email, password: this.password}
-    this.api.post('/merchant/auth', JSON.stringify(data)).then(response => {
-      console.log(response.data);
-      return response.data.token
-    }).catch(error => console.log(error))
+     return this.api.post('/auth', JSON.stringify(data))
   }
 
   airtimeInfo(phoneNumber) {
-    return this.api.get(`airtime/info/${phoneNumber}`, this.headers)
+    return this.api.get(`/airtime/info/${phoneNumber}`, this.headers())
   }
 
 }
